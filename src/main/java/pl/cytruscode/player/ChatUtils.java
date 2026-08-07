f718e430f961a5dd2349a72eeb0137b2bc40174c
poldekdev <poldek333@outlook.com>
2026-08-07 19:48:21 +0200

    Added sendTitle method

--- src/main/java/pl/cytruscode/player/ChatUtils.java
+++ src/main/java/pl/cytruscode/player/ChatUtils.java
@@ -1,9 +1,12 @@
 package pl.cytruscode.player;
 
+import net.kyori.adventure.audience.Audience;
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.title.Title;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
+import java.time.Duration;
 import java.util.List;
 
 public class ChatUtils {
@@ -11,5 +14,11 @@ public class ChatUtils {
         for (Player p : Bukkit.getOnlinePlayers()){
             p.sendMessage(annoucement);
         }
+    }
+    public static void sendTitle(Player p, Component title, Component subtitle, int fadeIn, int stay, int fadeOut){
+        Title.Times times = Title.Times.times(Duration.ofSeconds(fadeIn), Duration.ofSeconds(stay), Duration.ofSeconds(fadeOut));
+        Title titleObject = Title.title(title, subtitle, times);
+
+        p.showTitle(titleObject);
     }
 }
